ac9ae8f3deb5cac365e10fbef20915aa244848fe
kremlevmax <kremlevmax.webdev@icloud.com>
2023-02-24 14:05:29 -0800

    Add login page

--- myfirstspringwebapplication/src/main/java/com/kremlevmax/myfirstspringwebapplication/controllers/login/LoginController.java
+++ myfirstspringwebapplication/src/main/java/com/kremlevmax/myfirstspringwebapplication/controllers/login/LoginController.java
@@ -2,19 +2,38 @@ package com.kremlevmax.myfirstspringwebapplication.controllers.login;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	private AuthenticationService authenticationService;
 	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
+	@Autowired
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
 
 	@RequestMapping("/login")
-	public String loginController(@RequestParam String name, ModelMap model) {
-		logger.info(name+" is a request parameter");
-		model.addAttribute("name", name);
+	public String goToLoginPage() {
 		return "login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String goToWelcomePage(@RequestParam String login, @RequestParam String password, ModelMap model) {
+		if (authenticationService.verifyCredentials(login, password)) {
+			logger.info(login + " logged in");
+			model.addAttribute("login", login);
+			return "welcome";
+		} else {
+			return "login";
+		}
 	}
 }
--- myfirstspringwebapplication/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ myfirstspringwebapplication/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,7 +3,12 @@
 		<title>Login Page</title>
 	</head>
 	<body>
-		<h2>Login Page</h2>
-		Welcome ${name}
+		<form method="post">
+			<label for="login">Login:</label><br>
+			<input type="text" id="login" name="login"><br>
+			<label for="password">Password:</label><br>
+			<input type="password" id="password" name="password">
+			<input type="submit" value="Submit">
+		</form>
 	</body>
 </html>
